d55ea363e74ac346bee837fce9fd0787f528a03a
Tomasz Zawartko <tomasz.zawartko@gmail.com>
2020-06-14 15:57:57 +0200

    added cross origin

--- src/main/java/com/tzawartko/employee/rest/EmployeeRestController.java
+++ src/main/java/com/tzawartko/employee/rest/EmployeeRestController.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,7 +44,8 @@ public class EmployeeRestController {
     DirectorsCounterCache directorsCounterCache;
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping(value = "/{id}"/*, produces = MediaType.APPLICATION_JSON_VALUE*/)
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CrossOrigin(origins = "http://localhost:4200")
     public EmployeeTo findById(@PathVariable Long id) {
         try {
             return employeeService.findById(id);
